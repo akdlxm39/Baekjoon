6a502c284073116a32fb6c489e167e028b829ac4
akdlxm39 <zxcwldnd@gmail.com>
2024-09-04 23:51:18 +0900

    1665 성공(입출력 시간 단축)

--- 1655 가운데를 말해요.py
+++ 1655 가운데를 말해요.py
@@ -1,14 +1,17 @@
+import sys
+input = sys.stdin.readline
 import heapq
 n = int(input())
-left_heap = [-int(input())]
+l = [int(input()) for _ in range(n)]
+left_heap = [-l[0]]
 right_heap = []
 switch = True
-l = (int(input()) for _ in range(n-1))
-print(-left_heap[0])
-for x in l:
+answer = [-left_heap[0]]
+for x in l[1:]:
     if switch:
         heapq.heappush(right_heap, x if x>=-left_heap[0] else -heapq.heappushpop(left_heap, -x))
     else:
         heapq.heappush(left_heap, -x if x<=right_heap[0] else -heapq.heappushpop(right_heap, x))
     switch = not switch
-    print(-left_heap[0])
+    answer.append(-left_heap[0])
+print('\n'.join(map(str, answer)))
