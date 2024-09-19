853be3327003f16de8bd4cfcd9f9561ac5720db3
akdlxm39 <zxcwldnd@gmail.com>
2024-09-19 22:09:38 +0900

    <시간 복잡도>단계 성공 https://www.acmicpc.net/step/53

---
+++ 시간 복잡도/24267 알고리즘 수업 - 알고리즘의 수행 시간 6.py
@@ -0,0 +1,5 @@
+import sys
+input = sys.stdin.readline
+N = int(input())
+print(N * (N - 1) * (N - 2) // 2 // 3)
+print(3)
---
+++ 시간 복잡도/24313 알고리즘 수업 - 점근적 표기 1.py
@@ -0,0 +1,6 @@
+import sys
+input = sys.stdin.readline
+a, b = map(int, input().split())
+c = int(input())
+n0 = int(input())
+print(int(a * n0 + b <= c * n0 and a <= c))
