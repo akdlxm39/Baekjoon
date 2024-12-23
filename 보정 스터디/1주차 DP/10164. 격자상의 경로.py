a69a76bd851b7e01b56b2744d9082b86e8c1558c
akdlxm39 <zxcwldnd@gmail.com>
2024-12-23 16:11:26 +0900

    10164 성공

---
+++ 보정 스터디/1주차 DP/10164. 격자상의 경로.py
@@ -0,0 +1,22 @@
+import sys
+input = sys.stdin.readline
+
+def solve(x, y):
+    dp = [1]*y
+    for _ in range(x-1):
+        for i in range(1, y):
+            dp[i] = dp[i] + dp[i-1]
+    return dp[-1]
+
+
+def main():
+    N, M, K = map(int, input().split())
+    if K != 0:
+        x, y = (K-1)//M, (K-1)%M
+        print(solve(x+1, y+1) * solve(N-x, M-y))
+    else:
+        print(solve(N, M))
+
+
+if __name__ == "__main__":
+    main()
