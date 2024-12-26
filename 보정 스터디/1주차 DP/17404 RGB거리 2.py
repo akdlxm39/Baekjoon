55da0e613f82f2e9f6386580764416d4eea02426
akdlxm39 <zxcwldnd@gmail.com>
2024-12-26 22:54:58 +0900

    17404 수정

--- 보정 스터디/1주차 DP/17404 RGB거리 2.py
+++ 보정 스터디/1주차 DP/17404 RGB거리 2.py
@@ -3,10 +3,9 @@ input = sys.stdin.readline
 
 def main():
     N = int(input())
-    rgb1 = list(map(int, input().split()))
-    rgb2 = list(map(int, input().split()))
-    rr, rg, rb, gr, gg, gb, br, bg, bb = [(rgb1[i]+rgb2[j]) if i!=j else 10000 for i in range(3) for j in range(3)]
-    for i in range(2, N):
+    rr, gg, bb = map(int, input().split())
+    rg, rb, gr, gb, br, bg = (10000,) * 6
+    for i in range(1, N):
         r, g, b = map(int, input().split())
         rr, rg, rb = min(rg, rb) + r, min(rr, rb) + g, min(rr, rg) + b
         gr, gg, gb = min(gg, gb) + r, min(gr, gb) + g, min(gr, gg) + b
