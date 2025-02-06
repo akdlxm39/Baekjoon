b4ee007db7f9ff7e84274fdb26f43c3953951a86
akdlxm39 <zxcwldnd@gmail.com>
2025-02-06 18:55:50 +0900

    1922, 2146 성공

---
+++ 보정 스터디/7주차 graph/1922 네트워크 연결.py
@@ -0,0 +1,22 @@
+import sys
+input = sys.stdin.readline
+
+def main():
+    n, m = int(input()), int(input())
+    costs = sorted((tuple(map(int, input().split())) for _ in range(m)), key = lambda x : x[2])
+    root = [x for x in range(n+1)]
+    link = dict([(x, {x}) for x in range(1, n+1)])
+    ans = 0
+    for a, b, cost in costs:
+        if root[a] == root[b]:
+            continue
+        ans += cost
+        link[root[a]] |= (link[root[b]])
+        if len(link[root[a]]) == n:
+            break
+        for x in link[root[b]]:
+            root[x] = root[a]
+    print(ans)
+
+if __name__ == "__main__":
+    main()
