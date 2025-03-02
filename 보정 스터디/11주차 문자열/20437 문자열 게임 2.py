d450f50e80638ba705124b9ce9da417d22f1ff2c
akdlxm39 <zxcwldnd@gmail.com>
2025-03-02 14:49:09 +0900

    20437 성공

---
+++ 보정 스터디/11주차 문자열/20437 문자열 게임 2.py
@@ -0,0 +1,27 @@
+import sys
+input = sys.stdin.readline
+
+def main():
+    for _ in range(int(input())):
+        w = input().rstrip()
+        k = int(input())
+        ans1, ans2 = 10000, 0
+        if k == 1:
+            ans1 = ans2 = 1
+        else:
+            cp = [[] for _ in range(124)]
+            for i, c in enumerate(w):
+                cp[ord(c)].append(i)
+            for l in cp[97:]:
+                if len(l) < k:
+                    continue
+                width = [y-x+1 for x, y in zip(l, l[k-1:])]
+                ans1 = min(ans1, min(width))
+                ans2 = max(ans2, max(width))
+        if ans1 == 10000 or ans2 == 0:
+            print(-1)
+        else:
+            print(ans1, ans2)
+
+if __name__ == "__main__":
+    main()
