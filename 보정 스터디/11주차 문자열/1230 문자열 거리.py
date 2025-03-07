1eb06ee64b37fdfb7e4760fd8a4e325c5748f613
akdlxm39 <zxcwldnd@gmail.com>
2025-03-07 16:30:19 +0900

    1230 개선

--- 보정 스터디/11주차 문자열/1230 문자열 거리.py
+++ 보정 스터디/11주차 문자열/1230 문자열 거리.py
@@ -4,37 +4,27 @@ input = sys.stdin.readline
 def main():
     o = input().rstrip()
     n = input().rstrip()
-    dp = [[[0, False] for _ in range(len(n)+1)] for _ in range(len(o)+1)]
-    dp[0][0][1] = True
-    for i, x in enumerate(o, 1):
-        for j, y in enumerate(n, 1):
+    dp = [(0, True)] + [(0, False) for _ in range(len(n))]
+    for x in o:
+        tmp = [(0, False)]
+        mindp = 1000
+        for i, y in enumerate(n, 1):
+            if dp[i-1][1]:
+                mindp = min(mindp, dp[i-1][0])
             if x == y:
-                if dp[i-1][j-1][1]:
-                    dp[i][j][0] = dp[i-1][j-1][0]
-                    dp[i][j][1] = True
+                if dp[i-1][1]:
+                    tmp.append((dp[i-1][0], True))
+                elif mindp != 1000:
+                    tmp.append((mindp+1, True))
                 else:
-                    tmp = [x[0] for x in dp[i-1][:j-1] if x[1]]
-                    if tmp:
-                        dp[i][j][0] = min(tmp)+1
-                        dp[i][j][1] = True
-    for i, l in enumerate(dp):
-        for j, x in enumerate(l):
-            if i == 0 and j > 0:
-                print(n[j-1], end=' ')
-            elif j == 0 and i > 0:
-                print(o[i-1], end=' ')
+                    tmp.append((0, False))
             else:
-                print(('\033[90m' if x[1]==False else '') + str(x[0]) + '\033[0m', end=' ')
-        print()
-    ans = len(n)
-    for i in range(1, len(n)+2):
-        if i == len(n)+1:
-            if dp[-1][-1][1]:
-                ans = min(ans, dp[-1][-1][0])
-            break
-        if dp[-1][i][1]:
-            ans = min(ans, dp[-1][i][0]+1)
-    print(ans if ans!=len(n) else -1)
+                tmp.append((0, False))
+        dp = tmp
+    ans = [x + 1 for x, b in dp[:-1] if b]
+    if dp[-1][1]:
+        ans.append(dp[-1][0])
+    print(min(ans) if ans else -1)
 
 if __name__ == "__main__":
     main()
