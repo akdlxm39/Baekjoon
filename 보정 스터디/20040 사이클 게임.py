76e808190f3052cd2cdc3811b7dc00df52862475
akdlxm39 <zxcwldnd@gmail.com>
2025-03-14 15:58:16 +0900

    20040 성공

---
+++ 보정 스터디/20040 사이클 게임.py
@@ -0,0 +1,31 @@
+import sys
+sys.setrecursionlimit(10**9)
+input = sys.stdin.readline
+
+def find(roots, u):
+    if roots[u] == u:
+        return u
+    roots[u] = find(roots, roots[u])
+    return roots[u]
+
+def union(roots, u, v):
+    ur, vr = find(roots, u), find(roots, v)
+    if ur < vr:
+        roots[vr] = ur
+    else:
+        roots[ur] = vr
+    return ur == vr
+
+def main():
+    n, m = map(int, input().split())
+    roots = list(range(n))
+    for i in range(1, m+1):
+        u, v = map(int, input().split())
+        if union(roots, u, v):
+            print(i)
+            break
+    else:
+        print(0)
+
+if __name__ == "__main__":
+    main()
