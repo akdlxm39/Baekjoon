88855fe4b8c6f468a3a1a7998f794e60e6c6fe12
akdlxm39 <zxcwldnd@gmail.com>
2025-03-26 15:43:50 +0900

    2206 성공

---
+++ hi-arc study/2주차 well-known/2206 벽 부수고 이동하기.py
@@ -0,0 +1,31 @@
+import sys
+from collections import deque
+input = sys.stdin.readline
+
+def bfs(n, m, maps):
+    visited = [[0]*m for _ in range(n)]
+    queue = deque([(0, 0, 1, True)])
+    visited[0][0] = 2
+    while queue:
+        x, y, dist, can_break = queue.popleft()
+        for dx, dy in [(0,1),(1,0),(0,-1),(-1,0)]:
+            nx, ny = x+dx, y+dy
+            if not (0<=nx<n and 0<=ny<m) or visited[nx][ny] == 2:
+                continue
+            if nx == n-1 and ny == m-1:
+                return dist+1
+            if can_break:
+                visited[nx][ny] = 2
+                queue.append((nx, ny, dist+1, maps[nx][ny]==0))
+            elif visited[nx][ny]==0 and maps[nx][ny]==0:
+                visited[nx][ny] = 1
+                queue.append((nx, ny, dist+1, False))
+    return -1
+
+def main():
+    n, m = map(int, input().split())
+    maps = [list(map(int, list(input().rstrip()))) for _ in range(n)]
+    print(1 if n==1 and m==1 else bfs(n, m, maps))
+
+if __name__ == "__main__":
+    main()
