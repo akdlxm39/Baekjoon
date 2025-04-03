f9da7486bb73281dcec6fb35d762a31cc4412c6d
akdlxm39 <zxcwldnd@gmail.com>
2025-04-03 17:32:15 +0900

    17371 성공

---
+++ 보정 스터디/15주차 Geometry/17371 이사.py
@@ -0,0 +1,17 @@
+import sys
+input = sys.stdin.readline
+
+def main():
+    n = int(input())
+    amenities = [tuple(map(int, input().split())) for _ in range(n)]
+    ans = (0, 0)
+    length_min = 1000000000000
+    for ax, ay in amenities:
+        length_max = max((bx-ax)**2+(by-ay)**2 for bx, by in amenities)
+        if length_max < length_min:
+            length_min = length_max
+            ans = (ax, ay)
+    print(*ans)
+
+if __name__ == "__main__":
+    main()
