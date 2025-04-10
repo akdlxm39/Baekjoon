9306360682dc13ea5f09d43297d58ab4385155a4
akdlxm39 <zxcwldnd@gmail.com>
2025-04-10 21:21:01 +0900

    10216, 11664 성공

---
+++ 보정 스터디/16주차 Geometry/10216 Count Circle Groups.py
@@ -0,0 +1,26 @@
+import sys
+input = sys.stdin.readline
+
+def find(root, i):
+    if root[i] != i:
+        root[i] = find(root, root[i])
+    return root[i]
+
+def main():
+    for _ in range(int(input())):
+        n = int(input())
+        x, y, r = [0]*n, [0]*n, [0]*n
+        for i in range(n):
+            x[i], y[i], r[i] = map(int, input().split())
+        root = list(range(n))
+        for i in range(1, n):
+            for j in range(i):
+                if (r[i] + r[j]) * (r[i] + r[j]) >= (x[i] - x[j]) * (x[i] - x[j]) + (y[i] - y[j]) * (y[i] - y[j]):
+                    root[find(root, i)] = find(root, j)
+        ans = 0
+        for i in range(n):
+            ans += i==root[i]
+        print(ans)
+
+if __name__ == "__main__":
+    main()
