4363059bddd16d1d9ceb1edc73a549294bc45a69
akdlxm39 <zxcwldnd@gmail.com>
2025-04-12 18:10:25 +0900

    11438 성공

---
+++ 보정 스터디/17주차 Trees/11438 LCA 2.py
@@ -0,0 +1,44 @@
+import sys
+from collections import deque
+input = sys.stdin.readline
+
+def make_tree(n, adj_list, parents, depths):
+    queue = deque([1])
+    depths[1] = 0
+    while queue:
+        cur = queue.popleft()
+        for nxt in adj_list[cur]:
+            if depths[nxt] == -1:
+                parents[nxt][0] = cur
+                depths[nxt] = depths[cur]+1
+                queue.append(nxt)
+    for j in range(19):
+        for i in range(1, n+1):
+            parents[i][j+1] = parents[parents[i][j]][j]
+
+def main():
+    n = int(input())
+    adj_list = [[] for _ in range(n+1)]
+    for _ in range(n-1):
+        a, b = map(int, input().split())
+        adj_list[a].append(b)
+        adj_list[b].append(a)
+    parents = [[0] * 20 for _ in range(n + 1)]
+    depths = [-1] * (n + 1)
+    make_tree(n, adj_list, parents, depths)
+    m = int(input())
+    for _ in range(m):
+        a, b = map(int, input().split())
+        if depths[a] < depths[b]:
+            a, b = b, a
+        while depths[a] != depths[b]:
+            a = parents[a][len(bin(depths[a] - depths[b]))-3]
+        while a!=b:
+            i = len(bin(depths[a]))-2
+            while i > 0 and parents[a][i]==parents[b][i]:
+                i -= 1
+            a, b = parents[a][i], parents[b][i]
+        print(a)
+
+if __name__ == "__main__":
+    main()
